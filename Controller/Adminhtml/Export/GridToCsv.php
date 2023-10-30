cd104b58380cf72b73a6805f221e2b0fd973d3ea
indykoning <15870933+indykoning@users.noreply.github.com>
2023-10-30 17:06:04 +0100

    Added xls & xls export support.
    Co-authored-by: Alber Developer <alber.developer@gmail.com>

---
+++ Controller/Adminhtml/Export/GridToCsv.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JustBetter\ProductGridExport\Controller\Adminhtml\Export;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Response\Http\FileFactory;
+use JustBetter\ProductGridExport\Model\Export\ConvertToCsv;
+
+class GridToCsv extends Action
+{
+    /**
+     * @param Context $context
+     * @param ConvertToCsv $converter
+     * @param FileFactory $fileFactory
+     */
+    public function __construct(
+        Context $context,
+        protected ConvertToCsv $converter,
+        protected FileFactory $fileFactory
+    ){
+        parent::__construct($context);
+    }
+
+    /**
+     * Export data provider to CSV
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return \Magento\Framework\App\ResponseInterface
+     */
+    public function execute()
+    {
+        return $this->fileFactory->create('export.csv', $this->converter->getCsvFile(), 'var');
+    }
+
+}
---
+++ Controller/Adminhtml/Export/GridToXls.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JustBetter\ProductGridExport\Controller\Adminhtml\Export;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Response\Http\FileFactory;
+use JustBetter\ProductGridExport\Model\Export\ConvertToXls;
+
+class GridToXls extends Action
+{
+    /**
+     * @param Context $context
+     * @param ConvertToXls $converter
+     * @param FileFactory $fileFactory
+     */
+    public function __construct(
+        Context $context,
+        protected ConvertToXls $converter,
+        protected FileFactory $fileFactory
+    ){
+        parent::__construct($context);
+    }
+
+    /**
+     * Export data provider to CSV
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return \Magento\Framework\App\ResponseInterface
+     */
+    public function execute()
+    {
+        return $this->fileFactory->create('export.xls', $this->converter->getXlsFile(), 'var');
+    }
+
+}
